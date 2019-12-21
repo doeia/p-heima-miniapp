5dee9654987347aa8db95818e12c4b5826553453
sanshi1929 <sanshi2929@gmail.com>
2019-12-21 20:54:21 +0900

    '01search-swiper'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,28 @@
-//Page Object
+import { request } from "../../request/index.js";
 Page({
   data: {
+    swiperList: []
 
   },
   //options(Object)
   onLoad: function (options) {
+    // var reqTask = wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   },
+    //   fail: () => { },
+    //   complete: () => { }
+    // });
 
-  },
-  onReady: function () {
-
-  },
-  onShow: function () {
-
-  },
-  onHide: function () {
-
-  },
-  onUnload: function () {
-
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
-  },
-  onPageScroll: function () {
-
+    request({ url: "https://api.zbztb.cn/api/public/v1/home/swiperdata" })
+      .then(result => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      })
   },
   //item(index,pagePath,text)
   onTabItemTap: function (item) {
