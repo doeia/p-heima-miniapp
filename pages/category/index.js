3c1993ff48d51581dae733a8b720fce1022c4014
sanshi1929 <sanshi2929@gmail.com>
2019-12-22 10:38:14 +0900

    '03catepage'

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,82 @@
 // pages/category/index.js
+import { request } from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightContent: [],
+    //左侧被点击的菜单
+    currentIndex: 0,
+    //右侧滚动条回到顶部
+    scrollTop: 0
   },
+
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 缓存：
+    // 判断本地有数据否，没有过期就发送请求
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      this.getCates();
+    } else {
+      //有旧的数据 定义过期时间
+      if (Date.now() - Cates.time > 1000 * 10) {
+        //重新发送请求
+        console.log('重新发送请求')
+        this.getCates()
+      } else {
+        //可以使用的旧数据
+        console.log('可以使用的旧数据')
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        let rightContent = this.Cates[0].children;
 
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getCates() {
+    request({
+      url: 'https://api.zbztb.cn/api/public/v1/categories'
+    })
+      .then(res => {
+        this.Cates = res.data.message;
 
+        //把接口的数据存储到本地
+        wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
+
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        let rightContent = this.Cates[0].children;
+
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // 左侧菜单的点击事件
+  handleItemTap(e) {
+    const { index } = e.currentTarget.dataset;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent,
+      scrollTop: 0
+    })
 
   }
 })
