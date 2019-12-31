6c4555f19fa7f625e50e3f21e801913850c06623
sanshi1929 <sanshi2929@gmail.com>
2020-01-01 01:10:08 +0900

    '10feedback'

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -5,6 +5,116 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tabs: [
+      {
+        id: 0,
+        value: '体验问题',
+        isActive: true
+      },
+      {
+        id: 1,
+        value: '商品、商家投诉',
+        isActive: false
+      }
+    ],
+    //被选中的图片路径
+    chooseImgs: [],
+    //文本域的内容
+    textVal: ""
+  },
+  //外网的图片路径数组
+  UpLoadImgs: [],
+
+  handleTabsItemChange(e) {
+    const { index } = e.detail;
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
+  },
+
+  handleChooseImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        console.log(result)
+        this.setData({
+          //图片数组 进行拼接
+          chooseImgs: [...this.data.chooseImgs, ...result.tempFilePaths]
+        })
+      }
+    });
+  },
+  handleRemoveImg(e) {
+    const { index } = e.currentTarget.dataset
+    let { chooseImgs } = this.data;
+    chooseImgs.splice(index, 1);
+    this.setData({
+      chooseImgs
+    })
+  },
+  handleTextInput(e) {
+    this.setData({
+      textVal: e.detail.value
+    })
+
+  },
+  handleFromSubmit() {
+    const { textVal, chooseImgs } = this.data;
+    if (!textVal.trim()) {
+      wx.showToast({
+        title: '输入不合法',
+        mask: true
+      });
+      return;
+    }
+    //上传图片到服务器
+    wx.showLoading({
+      title: "正在上传中",
+      mask: true
+    });
+
+    //判断有没有图片要一起上传
+    if (chooseImgs.length != 0) {
+      chooseImgs.forEach((v, i) => {
+        wx.uploadFile({
+          url: 'https://images.ac.cn/Home/Index/UploadAction/',
+          filePath: v,
+          name: "file",
+          formData: {},
+          success: (result) => {
+            let url = JSON.parse(result.data).url;
+            this.UpLoadImgs.push(url);
+
+            //所有图片都上传完毕才触发
+            if (i === chooseImgs.length - 1) {
+              wx.hideLoading();
+
+              //把文本和图片提交到后台中， 这里不做演示
+
+              this.setData({
+                textVal: "",
+                chooseImgs: []
+              })
+
+              wx.navigateBack({
+                delta: 1
+              });
+            }
+          }
+        });
+      })
+    } else {
+      wx.hideLoading();
+      //只把文本提交到后台中， 这里不做演示
+      wx.navigateBack({
+        delta: 1
+      });
+    }
+
 
   },
 
