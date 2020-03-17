839da436fa249a33f420d01b1b1789939b8f940a
sanshi1929 <sanshi1929@gmail.com>
2020-03-17 19:05:37 +0900

    change api address

--- pages/category/index.js
+++ pages/category/index.js
@@ -2,7 +2,6 @@
 import { request } from "../../request/index.js";
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -20,7 +19,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 缓存：
     // 判断本地有数据否，没有过期就发送请求
     const Cates = wx.getStorageSync("cates");
@@ -30,11 +29,11 @@ Page({
       //有旧的数据 定义过期时间
       if (Date.now() - Cates.time > 1000 * 10) {
         //重新发送请求
-        console.log('重新发送请求')
-        this.getCates()
+        console.log("重新发送请求");
+        this.getCates();
       } else {
         //可以使用的旧数据
-        console.log('可以使用的旧数据')
+        console.log("可以使用的旧数据");
         this.Cates = Cates.data;
         let leftMenuList = this.Cates.map(v => v.cat_name);
         let rightContent = this.Cates[0].children;
@@ -42,29 +41,28 @@ Page({
         this.setData({
           leftMenuList,
           rightContent
-        })
+        });
       }
     }
   },
 
   getCates() {
     request({
-      url: 'https://api.zbztb.cn/api/public/v1/categories'
-    })
-      .then(res => {
-        this.Cates = res.data.message;
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    }).then(res => {
+      this.Cates = res.data.message;
 
-        //把接口的数据存储到本地
-        wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
+      //把接口的数据存储到本地
+      wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
 
-        let leftMenuList = this.Cates.map(v => v.cat_name);
-        let rightContent = this.Cates[0].children;
+      let leftMenuList = this.Cates.map(v => v.cat_name);
+      let rightContent = this.Cates[0].children;
 
-        this.setData({
-          leftMenuList,
-          rightContent
-        })
-      })
+      this.setData({
+        leftMenuList,
+        rightContent
+      });
+    });
   },
 
   // 左侧菜单的点击事件
@@ -76,7 +74,6 @@ Page({
       currentIndex: index,
       rightContent,
       scrollTop: 0
-    })
-
+    });
   }
-})
+});
